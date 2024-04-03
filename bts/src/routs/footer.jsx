768867eece0c3c528678f975576c81f5131e5082
AsyqD <sydykarnur@gmail.com>
2024-04-03 07:47:46 +0500

    footer added

---
+++ bts/src/routs/footer.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "../styles/Footer.css";
+import instImg from "../images/instagram.png";
+import twitterImg from "../images/twitter.png";
+import facebookImg from "../images/facebook.png";
+import whatsappImg from "../images/whatsapp.png";
+
+export default function Footer() {
+  return (
+    <footer>
+      <header className="footer-header">
+        <div className="miniheader-footer">
+          <h1>Contact us</h1>
+        </div>
+      </header>
+      <div className="footer-container">
+        <div className="talk-box">
+          <p>
+            Every project starts with a chat. Tell us about your idea and we'll
+            help you bring it to life.
+          </p>
+          <button>Tell us about your project</button>
+        </div>
+        <div className="contact-box">
+          <div className="contact-info">
+            <p>
+              <span>Email:</span> happyclient@gmail.com
+            </p>
+            <p>
+              <span>Phone:</span> +7-707-707-7007
+            </p>
+            <p>
+              <span>Address:</span> Almaty region, Karasai district. 040900,
+              city of Kaskelen, st. Abylai Khan 1/1
+            </p>
+          </div>
+          <div className="social-links">
+            <ul>
+              <li>
+                <a href="#">
+                  <img src={twitterImg} alt="Twitter" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={facebookImg} alt="Facebook" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={instImg} alt="Instagram" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={whatsappImg} alt="WhatsApp" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="div-20">
+        <p>© 2024 Happy Client. All rights reserved</p>
+      </div>
+    </footer>
+  );
+}
--- bts/src/routs/root.jsx
+++ bts/src/routs/root.jsx
@@ -1,56 +1,77 @@
-import { Outlet, Link, useLoaderData, } from "react-router-dom";
+import { Outlet, Link, useLoaderData } from "react-router-dom";
 import React from "react";
 // import Header from "../header";
-import '../styles/Root.css';
-import logo from '../images/logo.png';
+import "../styles/Root.css";
+import logo from "../images/logo.png";
 import About from "./about";
 import Services from "./services";
 import Reviews from "./reviews";
-import {Link as ScrollLink} from 'react-scroll';
+import { Link as ScrollLink } from "react-scroll";
 import Contacts from "./contacts";
-export default function Root(){
-    return (
-      <div className="flex flex-col gap-2">
-        {/* <div style={{position:'fixed', top:0, left:0, width: '100%', height: '10%', zIndex: 100000}}> */}
-          <header className="root-header">
-            <div className="logo-container">
-              <img src={logo} alt="Logo" className="logo" />
-              <span className="logo-text">Happy <br /> Client</span>
-            </div>
-            <nav className="nav-links">
-              <ul>
-                {/* reac-router */}
-                {/* <li><Link to="/about">About</Link></li>
+import Footer from "./footer";
+
+export default function Root() {
+  return (
+    <div className="flex flex-col gap-2 white">
+      {/* <div style={{position:'fixed', top:0, left:0, width: '100%', height: '10%', zIndex: 100000}}> */}
+      <header className="root-header">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+          <span className="logo-text">
+            Happy <br /> Client
+          </span>
+        </div>
+        <nav className="nav-links">
+          <ul>
+            {/* reac-router */}
+            {/* <li><Link to="/about">About</Link></li>
                 <li><Link to="/services">Services</Link></li>
                 <li><Link to="/reviews">Reviews</Link></li>
                 <li><Link o="/contacts">Contacts</Link></li> */}
-                {/* react-sroll */}
-                <li><ScrollLink to="About" smooth={true} duration={500}>About</ScrollLink></li>
-                <li><ScrollLink to="Services" smooth={true} duration={500}>Services</ScrollLink></li>
-                <li><ScrollLink to="Reviews" smooth={true} duration={500}>Reviews</ScrollLink></li>
-                <li><ScrollLink to="Contacts" smooth={true} duration={500}>Contacts</ScrollLink></li>
-              </ul>
-            </nav>
-            <div className="buttons">
-              <button className="sign-up">Sign up</button>
-              <button className="sign-in">Sign in</button>
-            </div>  
-          </header>
-        {/* </div> */}
-        <div className="content">
-          <div id="About">
-              <About/>
-          </div>
-          <div id="Services">
-              <Services/>
-          </div>
-          <div id="Reviews">
-              <Reviews/>
-          </div>
-          <div id="Contacts">
-              <Contacts/>
-          </div>
+            {/* react-sroll */}
+            <li>
+              <ScrollLink to="About" smooth={true} duration={500}>
+                About
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="Services" smooth={true} duration={500}>
+                Services
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="Reviews" smooth={true} duration={500}>
+                Reviews
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="Contacts" smooth={true} duration={500}>
+                Contacts
+              </ScrollLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="buttons">
+          <button className="sign-up">Sign up</button>
+          <button className="sign-in">Sign in</button>
+        </div>
+      </header>
+      {/* </div> */}
+      <div className="content">
+        <div id="About">
+          <About />
+        </div>
+        <div id="Services">
+          <Services />
+        </div>
+        <div id="Reviews">
+          <Reviews />
+        </div>
+        <div id="Contacts">
+          {/* <Contacts/> */}
+          <Footer />
         </div>
       </div>
-    );
-};
+    </div>
+  );
+}
